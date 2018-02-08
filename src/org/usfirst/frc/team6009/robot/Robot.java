2b2d2daa1e223e5e49df3cfa341360351454d7d2
Cyberheart <frcteam6009@gmail.com>
2018-02-08 17:25:33 -0500

    Implementing/Failing rotationPID

--- src/org/usfirst/frc/team6009/robot/Robot.java
+++ src/org/usfirst/frc/team6009/robot/Robot.java
@@ -12,9 +12,9 @@
 //|																 |
 //|**************************************************************|
 
+
 package org.usfirst.frc.team6009.robot;
 
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -25,6 +25,14 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.can.*;
+import edu.wpi.first.wpilibj.SendableBase;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDSource;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -34,36 +42,98 @@ import edu.wpi.first.wpilibj.Spark;
  * project.
  */
 public class Robot extends IterativeRobot {
+	
+	// Auto Modes Setup
 	private static final String kDefaultAuto = "Default";
 	private static final String kCustomAuto = "My Auto";
 	private String m_autoSelected;
+	
+	//Variables
+	final static double ENCODER_COUNTS_PER_INCH = 13.49;
+	
+	// Smartdashboard Chooser object for Auto modes
 	private SendableChooser<String> m_chooser = new SendableChooser<>();
 	
-	SpeedController leftFront, leftBack, rightFront, rightBack;
+	// SpeedController Object creations - Define all names of motors here
+	SpeedController leftFront, leftBack, rightFront, rightBack, gripper;
 	
 	// Speed controller group used for new differential drive class
 	SpeedControllerGroup leftChassis, rightChassis;
 	
+	// DifferentialDrive replaces the RobotDrive Class from previous years
 	DifferentialDrive chassis;
 	
+	// Joystick Definitions
 	Joystick driver;
-
+	
+	//Boolean for buttons
+	boolean aButton, bButton, yButton, xButton;
+	
+	// Analog Sensors
+	AnalogInput ultrasonic_yellow, ultrasonic_black;
+	Solenoid ultra_solenoid;
+	
+	// Encoders
+	Encoder leftEncoder, rightEncoder;
+	
+	// Gyro
+	ADXRS450_Gyro gyroscope;
+	
+	//PID Variables
+	double Kp;
+	double Ki;
+	double Kd;
+	PIDController rotationPID;
+	
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
 	 */
 	@Override
 	public void robotInit() {
+		// Adds all of our previously created auto modes into the smartdashboard chooser
 		m_chooser.addDefault("Default Auto", kDefaultAuto);
 		m_chooser.addObject("My Auto", kCustomAuto);
 		SmartDashboard.putData("Auto choices", m_chooser);
 		
+		
+		// Defines all the ports of each of the motors
+		leftFront = new Spark(0);
+		leftBack = new Spark(1);
+		rightFront = new Spark(2);
+		rightBack = new Spark(3);
+		gripper = new Spark(4);
+		
+		driver = new Joystick(0);
+		
+		// Defines the left and right SpeedControllerGroups for our DifferentialDrive class
 		leftChassis = new SpeedControllerGroup(leftFront, leftBack);
 		rightChassis = new SpeedControllerGroup(rightFront, rightBack);
 		
+		// Inverts the right side of the drive train to account for the motors being physically flipped
 		rightChassis.setInverted(true);
 		
+		// Defines our DifferentalDrive object with both sides of our drivetrain
 		chassis = new DifferentialDrive(leftChassis, rightChassis);
+		
+		// Set up port for Ultrasonic Distance Sensor
+		ultrasonic_yellow = new AnalogInput(0);
+		ultrasonic_black = new AnalogInput(1);
+		ultra_solenoid = new Solenoid(0);
+		
+		
+		// Set up Encoder ports
+		leftEncoder = new Encoder(0,1);
+		rightEncoder = new Encoder(2,3);
+		
+		//Gyroscope Setup
+		gyroscope = new ADXRS450_Gyro();
+		gyroscope.calibrate();
+		
+		//
+		//PIDSource gyroAngle = gyroscope.pidGet();
+		rotationPID = new PIDController(Kp, Ki, Kd, gyroscope, gripper);
+		rotationPID.setEnabled(true);
 		
 	}
 
@@ -78,28 +148,11 @@ public class Robot extends IterativeRobot {
 	 * the switch structure below with additional strings. If using the
 	 * SendableChooser make sure to add them to the chooser code above as well.
 	 */
+	
 	@Override
 	public void autonomousInit() {
 		m_autoSelected = m_chooser.getSelected();
-		// autoSelected = SmartDashboard.getString("Auto Selector",
-		// defaultAuto);
 		System.out.println("Auto selected: " + m_autoSelected);
-		
-		//FMS message about switch orientation
-		String gameData;
-		gameData = DriverStation.getInstance().getGameSpecificMessage();
-		//Test the alliance side for the first switch
-		if (gameData.charAt(0) == 'L') {
-			System.out.println("Left");
-		} else {
-			System.out.println("Right");
-		}
-		//Test the alliance side for the scale
-		if (gameData.charAt(0) == 'L') {
-			System.out.println("Left");
-		} else {
-			System.out.println("Right");
-		}
 	}
 
 	/**
@@ -123,6 +176,34 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void teleopPeriodic() {
+		ultra_solenoid.set(true);
+		resetEncoders();
+		gyroscope.reset();
+		//leftChassis.set(0.1);
+		//rightChassis.set(0.1);
+		chassis.arcadeDrive(driver.getX(), -driver.getY());
+		
+		aButton = driver.getRawButton(1);
+		bButton = driver.getRawButton(2);
+		xButton = driver.getRawButton(3);
+
+		SmartDashboard.putNumber("Ultrasonic Yellow Distance (cm):", getUltrasonicYellowDistance());
+		SmartDashboard.putNumber("Ultrasonic Black Distance (cm):", getUltrasonicBlackDistance());
+		
+		
+		if (bButton){
+			gripper.set(-0.4);
+		} 
+		else if (aButton) {
+			gripper.set(0.4);
+		} 
+		else {
+			gripper.set(0.4);
+		}
+		if(xButton){
+			rotationPID.setSetpoint(50);
+		}
+		System.out.println(rotationPID.get());
 	}
 
 	/**
@@ -130,5 +211,39 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void testPeriodic() {
+	}
+	
+	public void disabledPeriodic(){
+		System.out.println(rotationPID);
+	}
+	
+	public void resetEncoders(){
+		leftEncoder.reset();
+		rightEncoder.reset();
+	}
+	
+	public double getDistance(){
+		return ((double)(leftEncoder.get() + rightEncoder.get()) / (ENCODER_COUNTS_PER_INCH * 2));
+	}
+
+	public double getUltrasonicYellowDistance(){
+		// Calculates distance in centimeters from ultrasonic distance sensor
+		return (double)(((ultrasonic_yellow.getAverageVoltage()*1000)/238.095)+9.0); //accuracy of 2 millimeters ;)
+	}
+
+	public double getUltrasonicBlackDistance(){
+		// Calculates distance in centimeters from ultrasonic distance sensor
+		return (double)(((ultrasonic_black.getAverageVoltage()*1000)/9.4));
+	}
+
+	private void updateSmartDashboard() {
+		
+		SmartDashboard.putData("Gyro", gyroscope);
+		SmartDashboard.putNumber("Gyro Angle", gyroscope.getAngle());
+		SmartDashboard.putNumber("Gyro Rate", gyroscope.getRate());
+
+		SmartDashboard.putNumber("Left Encoder Count", leftEncoder.get());
+		SmartDashboard.putNumber("Right Encoder Count", rightEncoder.get());
+		SmartDashboard.putNumber("Encoder Distance", getDistance());
 	}
 }
